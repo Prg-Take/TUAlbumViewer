c917dced69474cb6c7c980c04e86b0883f6a34d2
Prg-Take <mac.kamimori.bowler@gmail.com>
2020-02-24 15:32:54 +0900

    Initial Commit

---
+++ TUAlbumViewer/TUAlbumViewer.h
@@ -0,0 +1,19 @@
+//
+//  TUAlbumViewer.h
+//  TUAlbumViewer
+//
+//  Created by Tadasuke Uema on 2020/02/24.
+//  Copyright © 2020 Tadasuke Uema. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TUAlbumViewer.
+FOUNDATION_EXPORT double TUAlbumViewerVersionNumber;
+
+//! Project version string for TUAlbumViewer.
+FOUNDATION_EXPORT const unsigned char TUAlbumViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TUAlbumViewer/PublicHeader.h>
+
+
